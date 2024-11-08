ffb87b68d3aa4fc2a0767f1c9a80d38a41ce1c3f
Matteen <81588234+silver-cpu@users.noreply.github.com>
2024-11-07 21:05:57 -0500

    Add files via upload

---
+++ greetd/sway.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if test -z "${XDG_RUNTIME_DIR}"; then
+  uid="$(id -u)"
+   export XDG_RUNTIME_DIR="/tmp/${uid}-runtime-dir"
+#   export XDG_RUNTIME_DIR="/tmp/swayruntimedir"
+  if ! test -d "${XDG_RUNTIME_DIR}"; then
+    mkdir "${XDG_RUNTIME_DIR}"
+    chmod 0700 "${XDG_RUNTIME_DIR}"
+  fi
+fi
+
+dbus-run-session sway
---
+++ sway-config-main/README.md
@@ -0,0 +1,3 @@
+# Gentoo Sway Related Configuration
+
+See: https://github.com/libreisaac/gentoo-installation-guide-resources
